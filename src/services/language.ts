5b6776c750dfd28a234f92b0671e6701f6a111b0
Ayomide Onibokun <ayo6706@gmail.com>
2024-08-06 03:33:15 +0100

    Implement get languages services

---
+++ src/services/language.ts
@@ -0,0 +1,15 @@
+import { LanguageDto } from "../dto/country.dto";
+import { CountryRepository } from "../repository/country/country.repository";
+import { failedPromise } from "./util";
+
+export default class LanguageService {
+  constructor(private readonly repo: CountryRepository) { }
+  async getLanguages(): Promise<LanguageDto[]> {
+    try {
+      const result = await this.repo.getLanguages()
+      return result
+    } catch (error: any) {
+      return failedPromise(error)
+    }
+  }
+}
