c7c501f1c9e83c0b56045c2bc18c7aa1839dc339
Ayomide Onibokun <ayo6706@gmail.com>
2024-08-06 04:29:10 +0100

    Register country, region, language and statistics service

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,3 +1,11 @@
-export interface Services {
+import CountryService from "./country";
+import LanguageService from "./language";
+import RegionService from "./region";
+import StatisticsService from "./statistics";
 
+export interface Services {
+    countryService: CountryService,
+    regionService: RegionService,
+    languageService: LanguageService
+    statisticsService: StatisticsService
 }
